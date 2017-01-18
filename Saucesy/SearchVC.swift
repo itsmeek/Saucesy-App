53df38d667bcbcfbfe461244f3dbd9ab5f1f510a
itsmeek <meekyacquah@gmail.com>
2017-01-18 10:22:26 -0800

    tab bar issue fixed

--- Saucesy/SearchVC.swift
+++ Saucesy/SearchVC.swift
@@ -11,6 +11,20 @@ import UIKit
 class SearchVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.red
+        view.backgroundColor = UIColor.white
+       styleComponents()
+    }
+    
+    func styleComponents(){
+        //Changes the title text Featured
+        navigationItem.title = "Search"
+        
+        if let nav = self.navigationController?.navigationBar{
+            nav.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white, NSFontAttributeName: UIFont(name: "Avenir-Medium", size: 17)!]
+        }
+        
+        //Makes navigation bar no see through
+        navigationController?.navigationBar.isTranslucent = false
+        
     }
 }
